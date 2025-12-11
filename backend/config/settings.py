723101422c10293c8c790c0eeda0f7d3d2bae88b
userMarcPaul <rjsanoy08@gmail.com>
2025-12-12 01:59:57 +0800

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,8 +10,8 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-change-this-key-for-production'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-change-this-key-for-production'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
